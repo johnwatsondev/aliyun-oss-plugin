c49d971c5e6d1e6d9f04251f062de73e3c56cf11
JohnWatsonDev <johnwatsondev.com@gmail.com>
2017-05-03 20:47:04 +0800

    init

---
+++ src/main/java/com/fit2cloud/jenkins/aliyunoss/Utils.java
@@ -0,0 +1,31 @@
+package com.fit2cloud.jenkins.aliyunoss;
+
+import hudson.Util;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class Utils {
+	public static final String FWD_SLASH = "/";
+
+	public static boolean isNullOrEmpty(final String name) {
+		boolean isValid = false;
+		if (name == null || name.matches("\\s*")) {
+			isValid = true;
+		}
+		return isValid;
+	}
+
+	public static String replaceTokens(AbstractBuild<?, ?> build,
+			BuildListener listener, String text) throws IOException,
+			InterruptedException {
+		String newText = null;
+		if (!isNullOrEmpty(text)) {
+			Map<String, String> envVars = build.getEnvironment(listener);
+			newText = Util.replaceMacro(text, envVars);
+		}
+		return newText;
+	}
+}
